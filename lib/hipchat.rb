955e496c40bda11d5ea0c7d18bea400a112444a1
david <dgleal@gmail.com>
2010-10-25 20:11:19 +0100

    Import initial functionality.

---
+++ lib/hipchat.rb
@@ -0,0 +1,48 @@
+require 'httparty'
+require 'ostruct'
+
+module HipChat
+  class Client
+    include HTTParty
+
+    base_uri 'http://api.hipchat.com/v1/rooms'
+    format :json
+
+    def initialize(token)
+      @token = token
+    end
+
+    def rooms
+      @rooms ||= self.class.get("/list", :query => {:auth_token => @token})['rooms'].
+        map { |r| Room.new(@token, r) }
+    end
+
+    def [](name)
+      rooms.find { |r| r.name == name }
+    end
+  end
+
+  class Room < OpenStruct
+    include HTTParty
+
+    base_uri 'http://api.hipchat.com/v1/rooms'
+    format :json
+
+    def initialize(token, params)
+      @token = token
+
+      super(params)
+    end
+
+    def send(from, message, notify = false)
+      self.class.post('/message',
+           :query => { :auth_token => @token },
+           :body => {
+             :room_id => room_id,
+             :from => from,
+             :message => message,
+             :notify => notify ? 1 : 0
+           })
+    end
+  end
+end
---
+++ lib/hipchat/capistrano.rb
@@ -0,0 +1,25 @@
+require 'hipchat'
+
+Capistrano::Configuration.instance(:must_exist).load do
+  namespace :hipchat do
+    task :set_hipchat_client do
+      set :hipchat_client, HipChat::Client.new(hipchat_token)
+    end
+
+    task :notify_deploy_started do
+      rails_env = fetch(:hipchat_env, fetch(:rails_env, "production"))
+
+      hipchat_client[hipchat_room_name].
+        send(hipchat_user, "Started deploying #{application} (#{rails_env}).", hipchat_announce)
+    end
+
+    task :notify_deploy_finished do
+      hipchat_client[hipchat_room_name].
+        send(hipchat_user, "Finished deploying #{application}.", hipchat_announce)
+    end
+  end
+
+  before "hipchat:notify_deploy_started", "hipchat:set_hipchat_client"
+  before "deploy", "hipchat:notify_deploy_started"
+  after  "deploy", "hipchat:notify_deploy_finished"
+end
