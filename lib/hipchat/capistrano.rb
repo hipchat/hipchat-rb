a70eb961aadd7288008036a5ea524257d3439fc4
Darren Boyd <dboyd@realgravity.com>
2012-01-25 17:39:42 -0800

    Calling hipchat send with latest usage.
    This avoids deprecation warnings on how the 'notify' option
    is passed.

--- lib/hipchat/capistrano.rb
+++ lib/hipchat/capistrano.rb
@@ -21,7 +21,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       if hipchat_send_notification
         on_rollback do
           hipchat_client[hipchat_room_name].
-            send(deploy_user, "#{human} cancelled deployment of #{deployment_name} to #{env}.", hipchat_announce)
+            send(deploy_user, "#{human} cancelled deployment of #{deployment_name} to #{env}.", :notify => hipchat_announce)
         end
 
         message = "#{human} is deploying #{deployment_name} to #{env}"
@@ -35,7 +35,7 @@ Capistrano::Configuration.instance(:must_exist).load do
 
     task :notify_deploy_finished do
       hipchat_client[hipchat_room_name].
-        send(deploy_user, "#{human} finished deploying #{deployment_name} to #{env}.", hipchat_announce)
+        send(deploy_user, "#{human} finished deploying #{deployment_name} to #{env}.", :notify => hipchat_announce)
     end
 
     def deployment_name
