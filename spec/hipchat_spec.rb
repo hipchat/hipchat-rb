8633966a81fd36cd7e9da18b0692657e639a1daf
Ralph Churchill <mrchucho@mrchucho.net>
2013-12-10 14:25:56 -0600

    Updated spec to cover v1 **and** v2.

--- spec/hipchat_spec.rb
+++ spec/hipchat_spec.rb
@@ -1,175 +1,235 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe HipChat do
-  subject { HipChat::Client.new("blah", :api_version => 'v2') }
-
-  let(:room) { subject["Hipchat"] }
-
-  # Helper for mocking room message post requests
-  def mock_successful_send(from, message, options={})
-    options = {:color => 'yellow', :notify => false, :message_format => 'html'}.merge(options)
-    mock(HipChat::Room).post("/Hipchat/notification",
-                             :query => {:auth_token => "blah"},
-                             :body  => {:room_id => "Hipchat",
-                                        :from    => "Dude",
-                                        :message => "Hello world",
-                                        :message_format => options[:message_format],
-                                        :color   => options[:color],
-                                        :notify  => options[:notify]}.to_json) {
-      OpenStruct.new(:code => 200)
-    }
-  end
-
-  def mock_successful_topic_change(topic, options={})
-    options = {:from => 'API'}.merge(options)
-    mock(HipChat::Room).put("/Hipchat/topic",
-                             :query => {:auth_token => "blah"},
-                             :body  => {:room_id => "Hipchat",
-                                        :from    => options[:from],
-                                        :topic   => "Nice topic" }.to_json ) {
-      OpenStruct.new(:code => 200)
-    }
-  end
-
-  def mock_successful_history(options={})
-    options = { :date => 'recent', :timezone => 'UTC', :format => 'JSON' }.merge(options)
-    canned_response = File.new File.expand_path(File.dirname(__FILE__) + '/example/history.json')
-    stub_request(:get, "https://api.hipchat.com/v2/room/Hipchat/history").with(:query => {:auth_token => "blah",
-                                   :room_id    => "Hipchat",
-                                   :date       => options[:date],
-                                   :timezone   => options[:timezone],
-                                   :format     => options[:format]}).to_return(canned_response)
-  end
-
-  describe "#history" do
-    it "is successful without custom options" do
-      mock_successful_history()
-
-      room.history().should be_true
+  %w(v1 v2).each do |api_version|
+    @api_version = api_version
+    def v1?
+      @api_version.eql?('v1')
     end
 
-    it "is successful with custom options" do
-      mock_successful_history(:timezone => 'America/Los_Angeles', :date => '2010-11-19')
-      room.history(:timezone => 'America/Los_Angeles', :date => '2010-11-19').should be_true
+    subject { HipChat::Client.new("blah", :api_version => api_version) }
+
+    let(:room) { subject["Hipchat"] }
+
+    # Helper for mocking room message post requests
+    def mock_successful_send(from, message, options={})
+      if v1?
+        options = {:color => 'yellow', :notify => 0, :message_format => 'html'}.merge(options)
+        mock(HipChat::Room).post("/message",
+                                 :query => {:auth_token => "blah"},
+                                 :body  => {:room_id => "Hipchat",
+                                            :from    => "Dude",
+                                            :message => "Hello world",
+                                            :message_format => options[:message_format],
+                                            :color   => options[:color],
+                                            :notify  => options[:notify]}) {
+          OpenStruct.new(:code => 200)
+        }
+      else
+        options = {:color => 'yellow', :notify => false, :message_format => 'html'}.merge(options)
+        mock(HipChat::Room).post("/Hipchat/notification",
+                                 :query => {:auth_token => "blah"},
+                                 :body  => {:room_id => "Hipchat",
+                                            :from    => "Dude",
+                                            :message => "Hello world",
+                                            :message_format => options[:message_format],
+                                            :color   => options[:color],
+                                            :notify  => options[:notify]}.to_json) {
+          OpenStruct.new(:code => 200)
+        }
+      end
     end
 
-    it "fails when the room doen't exist" do
-      mock(HipChat::Room).get(anything, anything) {
-        OpenStruct.new(:code => 404)
-      }
-
-      lambda { room.history }.should raise_error(HipChat::UnknownRoom)
+    def mock_successful_topic_change(topic, options={})
+      if v1?
+        options = {:from => 'API'}.merge(options)
+        mock(HipChat::Room).post("/topic",
+                                 :query => {:auth_token => "blah"},
+                                 :body  => {:room_id => "Hipchat",
+                                            :from    => options[:from],
+                                            :topic   => "Nice topic" } ) {
+          OpenStruct.new(:code => 200)
+        }
+      else
+        options = {:from => 'API'}.merge(options)
+        mock(HipChat::Room).put("/Hipchat/topic",
+                                 :query => {:auth_token => "blah"},
+                                 :body  => {:room_id => "Hipchat",
+                                            :from    => options[:from],
+                                            :topic   => "Nice topic" }.to_json ) {
+          OpenStruct.new(:code => 200)
+        }
+      end
     end
 
-    it "fails when we're not allowed to do so" do
-      mock(HipChat::Room).get(anything, anything) {
-        OpenStruct.new(:code => 401)
-      }
-
-      lambda { room.history }.should raise_error(HipChat::Unauthorized)
+    def mock_successful_history(options={})
+      if v1?
+        options = { :date => 'recent', :timezone => 'UTC', :format => 'JSON' }.merge(options)
+        canned_response = File.new File.expand_path(File.dirname(__FILE__) + '/example/history.json')
+        stub_request(:get, "https://api.hipchat.com/v1/rooms/history").with(:query => {:auth_token => "blah",
+                                       :room_id    => "Hipchat",
+                                       :date       => options[:date],
+                                       :timezone   => options[:timezone],
+                                       :format     => options[:format]}).to_return(canned_response)
+      else
+        options = { :date => 'recent', :timezone => 'UTC', :format => 'JSON' }.merge(options)
+        canned_response = File.new File.expand_path(File.dirname(__FILE__) + '/example/history.json')
+        stub_request(:get, "https://api.hipchat.com/v2/room/Hipchat/history").with(:query => {:auth_token => "blah",
+                                       :room_id    => "Hipchat",
+                                       :date       => options[:date],
+                                       :timezone   => options[:timezone],
+                                       :format     => options[:format]}).to_return(canned_response)
+      end
     end
 
-    it "fails if we get an unknown response code" do
-      mock(HipChat::Room).get(anything, anything) {
-        OpenStruct.new(:code => 403)
-      }
+    describe "#history (API #{api_version})" do
+      it "is successful without custom options" do
+        mock_successful_history()
 
-      lambda { room.history }.
-        should raise_error(HipChat::UnknownResponseCode)
-    end
-  end
+        room.history().should be_true
+      end
 
-  describe "#topic" do
-    it "is successful without custom options" do
-      mock_successful_topic_change("Nice topic")
+      it "is successful with custom options" do
+        mock_successful_history(:timezone => 'America/Los_Angeles', :date => '2010-11-19')
+        room.history(:timezone => 'America/Los_Angeles', :date => '2010-11-19').should be_true
+      end
 
-      room.topic("Nice topic").should be_true
-    end
+      it "fails when the room doen't exist" do
+        mock(HipChat::Room).get(anything, anything) {
+          OpenStruct.new(:code => 404)
+        }
 
-    it "is successful with a custom from" do
-      mock_successful_topic_change("Nice topic", :from => "Me")
+        lambda { room.history }.should raise_error(HipChat::UnknownRoom)
+      end
 
-      room.topic("Nice topic", :from => "Me").should be_true
-    end
+      it "fails when we're not allowed to do so" do
+        mock(HipChat::Room).get(anything, anything) {
+          OpenStruct.new(:code => 401)
+        }
 
-    it "fails when the room doesn't exist" do
-      mock(HipChat::Room).put(anything, anything) {
-        OpenStruct.new(:code => 404)
-      }
+        lambda { room.history }.should raise_error(HipChat::Unauthorized)
+      end
 
-      lambda { room.topic "" }.should raise_error(HipChat::UnknownRoom)
-    end
+      it "fails if we get an unknown response code" do
+        mock(HipChat::Room).get(anything, anything) {
+          OpenStruct.new(:code => 403)
+        }
 
-    it "fails when we're not allowed to do so" do
-      mock(HipChat::Room).put(anything, anything) {
-        OpenStruct.new(:code => 401)
-      }
-
-      lambda { room.topic "" }.should raise_error(HipChat::Unauthorized)
-    end
-
-    it "fails if we get an unknown response code" do
-      mock(HipChat::Room).put(anything, anything) {
-        OpenStruct.new(:code => 403)
-      }
-
-      lambda { room.topic "" }.
-        should raise_error(HipChat::UnknownResponseCode)
-    end
-  end
-
-  describe "sends a message to a room" do
-    it "successfully without custom options" do
-      mock_successful_send 'Dude', 'Hello world'
-
-      room.send("Dude", "Hello world").should be_true
+        lambda { room.history }.
+          should raise_error(HipChat::UnknownResponseCode)
+      end
     end
 
-    it "successfully with notifications on as option" do
-      mock_successful_send 'Dude', 'Hello world', :notify => true
+    describe "#topic (API #{api_version})" do
+      it "is successful without custom options" do
+        mock_successful_topic_change("Nice topic")
 
-      room.send("Dude", "Hello world", :notify => true).should be_true
+        room.topic("Nice topic").should be_true
+      end
+
+      it "is successful with a custom from" do
+        mock_successful_topic_change("Nice topic", :from => "Me")
+
+        room.topic("Nice topic", :from => "Me").should be_true
+      end
+
+      it "fails when the room doesn't exist" do
+        if v1?
+          mock(HipChat::Room).post(anything, anything) {
+            OpenStruct.new(:code => 404)
+          }
+        else
+          mock(HipChat::Room).put(anything, anything) {
+            OpenStruct.new(:code => 404)
+          }
+        end
+
+        lambda { room.topic "" }.should raise_error(HipChat::UnknownRoom)
+      end
+
+      it "fails when we're not allowed to do so" do
+        if v1?
+          mock(HipChat::Room).post(anything, anything) {
+            OpenStruct.new(:code => 401)
+          }
+        else
+          mock(HipChat::Room).put(anything, anything) {
+            OpenStruct.new(:code => 401)
+          }
+        end
+
+        lambda { room.topic "" }.should raise_error(HipChat::Unauthorized)
+      end
+
+      it "fails if we get an unknown response code" do
+        if v1?
+          mock(HipChat::Room).post(anything, anything) {
+            OpenStruct.new(:code => 403)
+          }
+        else
+          mock(HipChat::Room).put(anything, anything) {
+            OpenStruct.new(:code => 403)
+          }
+        end
+
+        lambda { room.topic "" }.
+          should raise_error(HipChat::UnknownResponseCode)
+      end
     end
 
-    it "successfully with custom color" do
-      mock_successful_send 'Dude', 'Hello world', :color => 'red'
+    describe "sends a message to a room (API #{api_version})" do
+      it "successfully without custom options" do
+        mock_successful_send 'Dude', 'Hello world'
 
-      room.send("Dude", "Hello world", :color => 'red').should be_true
-    end
+        room.send("Dude", "Hello world").should be_true
+      end
 
-    it "successfully with text message_format" do
-      mock_successful_send 'Dude', 'Hello world', :message_format => 'text'
+      it "successfully with notifications on as option" do
+        mock_successful_send 'Dude', 'Hello world', :notify => true
 
-      room.send("Dude", "Hello world", :message_format => 'text').should be_true
-    end
+        room.send("Dude", "Hello world", :notify => true).should be_true
+      end
 
-    it "but fails when the room doesn't exist" do
-      mock(HipChat::Room).post(anything, anything) {
-        OpenStruct.new(:code => 404)
-      }
+      it "successfully with custom color" do
+        mock_successful_send 'Dude', 'Hello world', :color => 'red'
 
-      lambda { room.send "", "" }.should raise_error(HipChat::UnknownRoom)
-    end
+        room.send("Dude", "Hello world", :color => 'red').should be_true
+      end
 
-    it "but fails when we're not allowed to do so" do
-      mock(HipChat::Room).post(anything, anything) {
-        OpenStruct.new(:code => 401)
-      }
+      it "successfully with text message_format" do
+        mock_successful_send 'Dude', 'Hello world', :message_format => 'text'
 
-      lambda { room.send "", "" }.should raise_error(HipChat::Unauthorized)
-    end
+        room.send("Dude", "Hello world", :message_format => 'text').should be_true
+      end
 
-    it "but fails if the username is more than 15 chars" do
-      lambda { room.send "a very long username here", "a message" }.should raise_error(HipChat::UsernameTooLong)
-    end
+      it "but fails when the room doesn't exist" do
+        mock(HipChat::Room).post(anything, anything) {
+          OpenStruct.new(:code => 404)
+        }
 
-    it "but fails if we get an unknown response code" do
-      mock(HipChat::Room).post(anything, anything) {
-        OpenStruct.new(:code => 403)
-      }
+        lambda { room.send "", "" }.should raise_error(HipChat::UnknownRoom)
+      end
 
-      lambda { room.send "", "" }.
-        should raise_error(HipChat::UnknownResponseCode)
+      it "but fails when we're not allowed to do so" do
+        mock(HipChat::Room).post(anything, anything) {
+          OpenStruct.new(:code => 401)
+        }
+
+        lambda { room.send "", "" }.should raise_error(HipChat::Unauthorized)
+      end
+
+      it "but fails if the username is more than 15 chars" do
+        lambda { room.send "a very long username here", "a message" }.should raise_error(HipChat::UsernameTooLong)
+      end
+
+      it "but fails if we get an unknown response code" do
+        mock(HipChat::Room).post(anything, anything) {
+          OpenStruct.new(:code => 403)
+        }
+
+        lambda { room.send "", "" }.
+          should raise_error(HipChat::UnknownResponseCode)
+      end
     end
   end
 
