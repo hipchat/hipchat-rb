08ae9541827ffa8b1cf5514aab926e308dc48c4b
Levi McCallum <levi@levimccallum.com>
2013-12-14 07:51:40 -0800

    Add capistrano 2 backwards support

--- lib/hipchat/capistrano.rb
+++ lib/hipchat/capistrano.rb
@@ -1,1 +1,5 @@
-load File.expand_path('capistrano/tasks/hipchat.rake', File.dirname(__FILE__))
+if defined?(Capistrano::VERSION) && Gem::Version.new(Capistrano::VERSION).release >= Gem::Version.new('3.0.0')
+  load File.expand_path('capistrano/tasks/hipchat.rake', File.dirname(__FILE__))
+else
+  require_relative 'capistrano2'
+end
