138103ea52e81f3e308dab248786e3a616bed414
Ken Dreyer <ktdreyer@ktdreyer.com>
2013-12-27 20:40:04 -0700

    require json in spec_helper
    When running the tests outside of rake or bundler, the tests fail
    because we don't require the json gem before calling `to_json`.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'hipchat'
 require 'rspec'
 require 'rspec/autorun'
+require 'json'
 require 'webmock/rspec'
 require 'coveralls'
 
